f1f85ad65918c58eb65045bf9f97e171e75295ae
RShields <playnow254@gmail.com>
2021-08-28 13:30:42 -0700

    Move to slash commands
    Also bumps versions and changes to tensorflow-cpu

--- bot.py
+++ bot.py
@@ -1,11 +1,13 @@
+import urllib.request
 from PIL import Image
-from urllib.request import urlopen
-import numpy as np
+from io import BytesIO
 import tensorflow as tf
 from tensorflow import keras
-import discord
+import numpy as np
+
 import os
-from io import BytesIO
+import discord
+from discord_slash import SlashCommand
 
 img_file = None
 
@@ -21,16 +23,16 @@ model.load_weights("fountain_on.h5")
 
 def get_prediction():
     global img_file
-    u = urlopen("https://www.washington.edu/cambots/camera1_l.jpg")
+    u = urllib.request.urlopen("https://www.washington.edu/cambots/camera1_l.jpg")
     img = Image.open(u)
     
     img_bytes = BytesIO()
     img.save(img_bytes, "JPEG")
     img_bytes.seek(0)
-    img_file = discord.File(fp=img_bytes,filename="circlepond.jpg")
+    img_file = discord.File(fp=img_bytes, filename="circlepond.jpg")
     
     img = img.crop((421, 249, 451, 309))
-    img_arr = np.reshape(img, (1,60,30,3))
+    img_arr = np.reshape(img, (1, 60, 30, 3))
     img_arr = np.multiply(img_arr, 1. / 255)
     o = model.predict(img_arr)
     #print(o)
@@ -38,25 +40,30 @@ def get_prediction():
 
 # bot stuff
 TOKEN = os.environ.get("TOKEN")
-client = discord.Client()
+bot = discord.Client(intents=discord.Intents.default())
+slash = SlashCommand(bot)
 
-@client.event
-async def on_message(message):
-    if message.author is client.user:
-        return
-    if message.content.startswith("!circlepond"):
-        predict_prob = get_prediction()
-        prediction = "ON" if predict_prob > 0.5 else "OFF"
-        predict_percent = "{0:.2%}".format(predict_prob if predict_prob > 0.5 else 1-predict_prob)
-        e = discord.Embed(
-            title="**I think Drumheller Fountain is {} ({} sure)**".format(prediction, predict_percent),
-            color=discord.Colour.from_rgb(51, 0, 111)
-        )
-        await message.channel.send(file=img_file,embed=e)
+def get_embed():
+    predict_prob = get_prediction()
+    prediction = "ON" if predict_prob > 0.5 else "OFF"
+    predict_percent = "{0:.2%}".format(predict_prob if predict_prob > 0.5 else 1-predict_prob)
+    
+    return discord.Embed(
+        title="**I think Drumheller Fountain is {} ({} sure)**".format(prediction, predict_percent),
+        color=discord.Colour.from_rgb(51, 0, 111)
+    )
 
-@client.event
+@bot.event
 async def on_ready():
-    activity = discord.Activity(name='!circlepond', type=discord.ActivityType.watching)
-    await client.change_presence(activity=activity)
+    activity = discord.Activity(name='/circlepond', type=discord.ActivityType.watching)
+    await bot.change_presence(activity=activity)
+    print("Running :)")
 
-client.run(TOKEN)
+@slash.slash(
+    name="circlepond",
+    description="Figure out if Drumheller Fountain is on")
+async def circlepond(ctx):
+    e = get_embed()
+    await ctx.send(file=img_file, embed=e)
+
+bot.run(TOKEN)
